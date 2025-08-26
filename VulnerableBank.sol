5c4942123893e8722d6bfd541ab876f8963d7d8a
Dulce <nubessweet@gmail.com>
2025-08-25 20:24:09 -0700

    Update VulnerableBank.sol

--- VulnerableBank.sol
+++ VulnerableBank.sol
@@ -1,1 +1,21 @@
 
+// VulnerableBank.sol
+pragma solidity ^0.8.0;
+
+contract VulnerableBank {
+    mapping(address => uint256) public balances;
+
+    function deposit() public payable {
+        balances[msg.sender] += msg.value;
+    }
+
+    // FUNCIÓN VULNERABLE
+    function withdraw(uint256 amount) public {
+        require(balances[msg.sender] >= amount, "Saldo insuficiente");
+        
+        (bool success, ) = msg.sender.call{value: amount}("");
+        require(success, "Transferencia fallida");
+        
+        balances[msg.sender] -= amount; 
+    }
+}
